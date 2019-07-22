8080043643e63543752d5628f345f8469939b707
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2019-07-21 22:48:01 -0400

    wrote basic views

--- kit-kat-project/kitkat/urls.py
+++ kit-kat-project/kitkat/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:req_id>/', views.detail, name='detail'),
+    path('new', views.new, name='new')
 ]
--- kit-kat-project/kitkat/views.py
+++ kit-kat-project/kitkat/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the kit kat index.")
+
+
+def detail(request, req_id):
+    response = f"You're looking at request {req_id}"
+    return HttpResponse(response)
+
+
+def new(request):
+    return HttpResponse("You're looking at the new request page")
+
+
