5bb178450252f3e1e130af926689d759b1ed72bd
Nancy Eckenthal <neckenth@Nancys-MacBook-Pro.local>
2019-07-22 22:58:02 -0400

    models defined - wip model methods and tests

--- kit-kat-project/kitkat/apps.py
+++ kit-kat-project/kitkat/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class KitkatConfig(AppConfig):
     name = 'kitkat'
+
+    def ready(self):
+        import kitkat.signals
---
+++ kit-kat-project/kitkat/signals.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch.dispatcher import receiver
+
+from kitkat.models import Profile
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, first_name=instance.first_name, last_name=instance.last_name)
