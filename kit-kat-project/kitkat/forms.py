a8da48df4521566fb149f9733ab7269e80c804d4
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2019-07-27 12:00:09 -0400

    starting timeoff request piece

--- kit-kat-project/kitkat/forms.py
+++ kit-kat-project/kitkat/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 from .models import TEAM_CHOICES, USER_TYPE_CHOICES
@@ -18,3 +19,8 @@ class SignUpForm(UserCreationForm):
             "username", "password1", "password2", "first_name", "last_name",
             "email", "team", "user_type"
         )
+
+
+class TimeOffRequestForm(ModelForm):
+    
+
